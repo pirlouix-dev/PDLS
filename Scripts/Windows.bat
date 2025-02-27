00528be62c3b4fd518e833b01fe6823ec10b4961
Mathéo Gomez <matheogomez@macbookpro.home>
2025-02-27 14:28:23 +0100

    Updated windows script

--- Scripts/Windows.bat
+++ Scripts/Windows.bat
@@ -4,8 +4,8 @@ setlocal ENABLEEXTENSIONS
 :: Variables
 set INSTALLER_URL=https://raw.githubusercontent.com/pirlouix-dev/PDLS/refs/heads/main/Installers/Windows.exe
 set INSTALLER_PATH=%TEMP%\PDLS_Installer.exe
-set APP_PATH=%ProgramFiles%\Plat de la Semaine\PlatDeLaSemaine.exe
-set APP_PARENT_PATH=%ProgramFiles%\Plat de la Semaine
+set APP_PATH=%AppData%\Plat de la Semaine\PlatDeLaSemaine.exe
+set APP_PARENT_PATH=%AppData%\Plat de la Semaine
 
 :: Colors
 set GREEN=[92m
