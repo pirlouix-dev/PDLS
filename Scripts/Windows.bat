6ca27d394e075bbdda804716c8745bb4ed15c0d7
pirlouix-dev <matheo.gomez34@gmail.com>
2025-02-28 10:10:05 +0100

    Gave up char problem

--- Scripts/Windows.bat
+++ Scripts/Windows.bat
@@ -8,25 +8,20 @@ set "INSTALLER_LOCATION=%TEMP%\PDLS_Installer.exe"
 set "APP_PATH=%AppData%\Plat de la Semaine\Plat de la Semaine.exe"
 set "APP_PARENT_PATH=%AppData%\Plat de la Semaine"
 
-:: Couleurs (si ANSI est supporté, sinon laisser vide)
-set "GREEN_START="
-set "RED_START="
-set "COLOR_STOP="
-
-echo %GREEN_START%Téléchargement de Plat de la Semaine%COLOR_STOP%
+echo Telechargement de Plat de la Semaine
 ::powershell -Command "Invoke-WebRequest -Uri '%INSTALLER_URL%' -OutFile '%INSTALLER_LOCATION%'" 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%INSTALLER_URL%', '%INSTALLER_LOCATION%')"
 set "ERR=%errorlevel%"
-call :check_error "Échec du téléchargement. Vérifiez votre connexion Internet." %ERR%
+call :check_error "Echec du telechargement. Verifiez votre connexion Internet." %ERR%
 
 if not exist "%INSTALLER_LOCATION%" (
-    echo %RED_START%Le fichier installé est introuvable. Veuillez réessayer.%COLOR_STOP%
+    echo Le fichier installe est introuvable. Veuillez reessayer.
     exit /b 1
 )
 
-echo %GREEN_START%Installation de l'application%COLOR_STOP%
+echo Installation de l'application
 
-:: Vérifier si l'application est ouverte
+:: Vrifier si l'application est ouverte
 tasklist /FI "IMAGENAME eq Plat de la Semaine.exe" 2>NUL | find /I "Plat de la Semaine.exe" >NUL
 if %errorlevel%==0 (
     taskkill /IM "Plat de la Semaine.exe" /F >nul 2>&1
@@ -54,7 +49,7 @@ if not exist "%APP_PARENT_PATH%" (
 :: Installation de l'application (copie du .exe téléchargé)
 copy /Y "%INSTALLER_LOCATION%" "%APP_PATH%" >NUL
 set "ERR=%errorlevel%"
-call :check_error "Échec de l'installation. Veuillez réessayer." %ERR%
+call :check_error "Echec de l'installation. Veuillez reessayer." %ERR%
 
 :: Suppression du fichier d'installation temporaire
 del /F /Q "%INSTALLER_LOCATION%"
@@ -63,12 +58,12 @@ del /F /Q "%INSTALLER_LOCATION%"
 start "" "%APP_PATH%"
 powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Plat de la Semaine.lnk'); $s.TargetPath='%APP_PATH%'; $s.WorkingDirectory='%APP_PARENT_PATH%'; $s.IconLocation='%APP_PATH%'; $s.Save()"
 
-echo %GREEN_START%Plat de la Semaine a été correctement installé%COLOR_STOP%
+echo Plat de la Semaine a ete correctement installe
 exit /b 0
 
 :check_error
 if not "%~2"=="0" (
-    echo %RED_START%%~1%COLOR_STOP%
+    echo Erreur: %~1
     exit /b 1
 )
 goto :eof
