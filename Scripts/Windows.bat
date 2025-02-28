d5ce713885534bf73a8212315b24eb926a9232d5
pirlouix-dev <matheo.gomez34@gmail.com>
2025-02-28 08:25:41 +0100

    Added shortucut

--- Scripts/Windows.bat
+++ Scripts/Windows.bat
@@ -4,13 +4,10 @@ setlocal EnableDelayedExpansion
 :: Variables
 set "INSTALLER_URL=https://raw.githubusercontent.com/pirlouix-dev/PDLS/refs/heads/main/Installers/Windows.exe"
 set "INSTALLER_LOCATION=%TEMP%\PDLS_Installer.exe"
-set "APP_PATH=%AppData%\Plat de la Semaine\Plat De La Semaine.exe"
+set "APP_PATH=%AppData%\Plat de la Semaine\Plat de la Semaine.exe"
 set "APP_PARENT_PATH=%AppData%\Plat de la Semaine"
 
 :: Couleurs (si ANSI est supporté, sinon laisser vide)
-::set "GREEN_START=<ESC>[92m"
-::set "RED_START=<ESC>[91m"
-::set "COLOR_STOP=<ESC>[0m"
 set "GREEN_START="
 set "RED_START="
 set "COLOR_STOP="
@@ -29,7 +26,7 @@ if not exist "%INSTALLER_LOCATION%" (
 echo %GREEN_START%Installation de l'application%COLOR_STOP%
 
 :: Fermeture de l'application si elle est en cours d'exécution
-taskkill /IM "Plat De La Semaine.exe" /F >nul 2>&1
+taskkill /IM "Plat de la Semaine.exe" /F >nul 2>&1
 
 :: Suppression de l'ancienne version si elle existe
 if exist "%APP_PATH%" (
@@ -43,19 +40,17 @@ if not exist "%APP_PARENT_PATH%" (
     mkdir "%APP_PARENT_PATH%"
 )
 
-:: Installation de l'application (copie du .exe téléchargé) sans afficher "1 fichier(s) copié(s)"
-copy /Y /Q "%INSTALLER_LOCATION%" "%APP_PATH%"
+:: Installation de l'application (copie du .exe téléchargé)
+copy /Y "%INSTALLER_LOCATION%" "%APP_PATH%"
 set "ERR=%errorlevel%"
 call :check_error "Échec de l'installation. Veuillez réessayer." %ERR%
-
-:: Création d'un raccourci dans le menu Démarrer pour qu'il soit repérable via la recherche Windows
-powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut(\"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Plat de la Semaine.lnk\"); $s.TargetPath=\"%APP_PATH%\"; $s.WorkingDirectory=\"%APP_PARENT_PATH%\"; $s.IconLocation=\"%APP_PATH%\"; $s.Save()"
 
 :: Suppression du fichier d'installation temporaire
 del /F /Q "%INSTALLER_LOCATION%"
 
 :: Lancement de l'application
 start "" "%APP_PATH%"
+powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Plat de la Semaine.lnk'); $s.TargetPath='%APP_PATH%'; $s.WorkingDirectory='%APP_PARENT_PATH%'; $s.IconLocation='%APP_PATH%'; $s.Save()"
 
 echo %GREEN_START%Plat de la Semaine a été correctement installé%COLOR_STOP%
 exit /b 0
